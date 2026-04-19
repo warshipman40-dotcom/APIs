4c5047e54dab042d59e3b3887c9d53f6d9a7368e
Charlie Yan <warshipman40@gmail.com>
2026-04-19 18:34:29 -0400

    uses github API and stores key information in lists. visualizes this data using plotly, currently working on adding a link to the bar graph using html

--- python_repos.py
+++ python_repos.py
@@ -1,5 +1,5 @@
 import requests
-
+import plotly.express as px
 #make an API call
 #assigns the URL of the API call to the variable
 url = "https://api.github.com/search/repositories"
@@ -24,11 +24,65 @@ print(f"Total Repositories: {response_dict["total_count"]}")
 print(f"Complete results : {not response_dict["incomplete_results"]}")
 
 #explore information about the repositories
+#the value associated with items is a list containing a number of dictionaries
+#each dictionary contains data about a repository
 repo_dicts = response_dict["items"]
+#we print this to see how many repositories we have info for
 print(f"Repositories returned: {len(repo_dicts)}")
 
 #examines the first repository
 repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-for key in sorted(repo_dict.keys()):
-    print(key)
+#prints the number of keys to see how much information we have
+#print(f"\nKeys: {len(repo_dict)}")
+#prints each individual key to see what kind of information is included
+#for key in sorted(repo_dict.keys()):
+    #print(key)
+
+# print("\nSelected information about repository one")
+# for repo_dict in repo_dicts:
+#     print(f"Name: {repo_dict["name"]}")
+#     #we use the key owner to access the dictionary representing the owner
+#     #then we use the key dictionary to access the login name
+#     print(f"Owner: {repo_dict["owner"]["login"]}")
+#     #we access the number of stars the project has earned
+#     print(f"Stars : {repo_dict["stargazers_count"]}")
+#     #we get the creation date
+#     print(f"Created: {repo_dict["created_at"]}")
+#     #we get the updated date
+#     print(f"Updated : {repo_dict["updated_at"]}")
+#     #we also get the description of the repository
+#     print(f"Description : {repo_dict["description"]}")
+
+#process repository information
+#three lists that store the names, number of stars, and hover_texts
+repo_links, stars, hover_texts = [], [], []
+for repo_dict in repo_dicts:
+    #adds the information to the lists
+    stars.append(repo_dict["stargazers_count"])
+    #turn repo names into active links
+    repo_name = repo_dict["name"]
+    repo_url = repo_dict["html_url"]
+    repo_link = f"<a href = '{repo_url}'>{repo_name} </a>"
+    repo_links.append(repo_link)
+    #Build hover texts
+    #pulls the owner and description from the dictionaries
+    owner = repo_dict["owner"]["login"]
+    descrption = repo_dict["description"]
+    #plotly allows you to use HTML code within text elements 
+    #we use <br /> for a new line
+    hover_text = f"{owner} <br /> {descrption}"
+    #this labels is appended to out list
+    hover_texts.append(hover_text)
+
+#creates a bar graph of the most popular APIS
+title = "Most-Starred Python Projects on GitHub"
+#labels of the graph
+labels = {"x" : "Repository Name ", "y" : "Stars "}
+#passes in the repository names, the number of stars, and label names
+#label names are added to the bars
+#hover_name takes in a single string
+#uses 
+fig = px.bar(x = repo_links, y = stars, labels = labels, hover_name = hover_texts)
+#changes font sizes using update_layout()
+fig.update_layout(title_font_size = 28, xaxis_title_font_size = 20, yaxis_title_font_size = 20)
+fig.show()
